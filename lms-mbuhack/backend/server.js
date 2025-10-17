4fcda90b416609255eb4d1392cfae8b489e3b00e
Jaideep811 <tjaideep821@gmail.com>
2025-10-18 05:05:47 +0530

    Refactor server.js to include database connection

--- lms-mbuhack/backend/server.js
+++ lms-mbuhack/backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const mongoose =require('express');
 const cors = require('cors');
 require('dotenv').config();
+const connectDB = require('./config/database');
 
 
 // Connect to database
